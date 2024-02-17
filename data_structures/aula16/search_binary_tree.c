c33135589cca026e955c72261943edf83ecc799f
Vaeep <diogobonofre@gmail.com>
2024-02-17 03:44:44 -0300

    Additions to aula16 codes

--- data_structures/aula16/search_binary_tree.c
+++ data_structures/aula16/search_binary_tree.c
@@ -24,24 +24,64 @@ NODE* create_node(KEY_TYPE value) {
 	return new;
 }
 
-NODE* insert_node(NODE* root, KEY_TYPE value) {
+NODE* insert(NODE* root, KEY_TYPE value) {
 	if (root == NULL) return create_node(value);
 	if (value < root->value) {
-		root->left = insert_node(root->left, value);
+		root->left = insert(root->left, value);
 	} else {
-		root->right = insert_node(root->right, value);
+		root->right = insert(root->right, value);
 	}
 	return root;
+}
+
+NODE* search(NODE* root, KEY_TYPE value) {
+	if (root == NULL) return NULL;
+	if (root->value == value) return root;
+	if (value < root->value) return search(root->left, value);
+	return search(root->right, value);
+}
+
+int size(NODE* root) {
+	if (root == NULL) return 0;
+	return size(root->left) + 1 + size(root->right);
+}
+
+void print(NODE* root) {
+	if (root != NULL) {
+		printf("%d", root->value);
+		printf("(");
+		print(root->left);
+		print(root->right);
+		printf(")");
+	}
 }
 
 int main(void) {
 	NODE* tree = new_tree();
 
-	tree = insert_node(tree, 15);
-	tree = insert_node(tree, 20);
-	tree = insert_node(tree, 23);
-	tree = insert_node(tree, 19);
-	tree = insert_node(tree, 8);
-	tree = insert_node(tree, 3);
-	tree = insert_node(tree, 12);
+	tree = insert(tree, 15);
+	tree = insert(tree, 20);
+	tree = insert(tree, 23);
+	tree = insert(tree, 19);
+	tree = insert(tree, 8);
+	tree = insert(tree, 3);
+	tree = insert(tree, 12);
+
+	print(tree);
+	printf("\n");
+	
+	bool r;
+	if (search(tree, 23)) r = true;
+	else r = false;
+	printf("Contain 23: %b\n", r);
+
+	if (search(tree, 99)) r = true;
+	else r = false;
+	printf("Contain 99: %b\n", r);
+	
+	if (search(tree, 89)) r = true;
+	else r = false;
+	printf("Contain 89: %b\n", r);
+
+	printf("Size: %d\n", size(tree));
 } 
