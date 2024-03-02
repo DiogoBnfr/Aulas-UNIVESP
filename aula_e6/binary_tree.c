76aa4cd24f16e738215d56d645b05eacca9b5968
Vaeep <diogobonofre@gmail.com>
2024-03-01 21:25:00 -0300

    Correction in aula_e6 codes

--- aula_e6/binary_tree.c
+++ aula_e6/binary_tree.c
@@ -9,47 +9,49 @@ typedef int KEY_TYPE;
 typedef enum { left, right } SIDE;
 
 typedef struct aux {
-	KEY_TYPE value;
-	struct aux *left, *right;
+  KEY_TYPE value;
+  struct aux *left, *right;
 } NODE;
 
 typedef struct {
-	NODE* root;
+  NODE *root;
 } BINARY_TREE;
 
-NODE* search_key(KEY_TYPE key, NODE* root) {
-	if (root == NULL) return NULL;
-	if (root->value == key) return root;
-	NODE* aux = search_key(key, root->left);
-	if (aux) return aux;
-	return search_key(key, root->right);
+NODE *search_key(KEY_TYPE key, NODE *root) {
+  if (root == NULL)
+    return NULL;
+  if (root->value == key)
+    return root;
+  NODE *aux = search_key(key, root->left);
+  if (aux)
+    return aux;
+  return search_key(key, root->right);
 }
 
-NODE* create_node(KEY_TYPE key) {
-	NODE* new = (NODE*)malloc(sizeof(NODE));
-	new->value = key;
-	new->left = NULL;
-	new->right = NULL;
-	return new;
+NODE *create_node(KEY_TYPE key) {
+  NODE *new = (NODE *)malloc(sizeof(NODE));
+  new->value = key;
+  new->left = NULL;
+  new->right = NULL;
+  return new;
 }
 
-void new_root(NODE** root, KEY_TYPE key) {
-	*root = create_node(key);
-}
+void new_root(NODE **root, KEY_TYPE key) { *root = create_node(key); }
 
-bool new_child(NODE* root, KEY_TYPE parent_key, KEY_TYPE key, SIDE s) {
-	NODE* parent = search_key(parent_key, root);
-	if (parent == NULL) return false;
-	NODE* new = create_node(key);
-	switch (s) {
-		case right:
-			new->right = parent->right;
-			parent->right = new;
-			break;
-		default:
-			new->left = parent->left;
-			parent->left = new;
-			break;
-	}
-	return true;
+bool new_child(NODE *root, KEY_TYPE parent_key, KEY_TYPE key, SIDE s) {
+  NODE *parent = search_key(parent_key, root);
+  if (parent == NULL)
+    return false;
+  NODE *new = create_node(key);
+  switch (s) {
+  case right:
+    new->right = parent->right;
+    parent->right = new;
+    break;
+  default:
+    new->left = parent->left;
+    parent->left = new;
+    break;
+  }
+  return true;
 }
