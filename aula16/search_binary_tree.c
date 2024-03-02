ade4619faf5feba42ab1f2d4516ddffb6594a45f
Vaeep <diogobonofre@gmail.com>
2024-03-01 21:13:32 -0300

    Implemented two ways of delete in SB Tree, aula16 codes

--- aula16/search_binary_tree.c
+++ aula16/search_binary_tree.c
@@ -7,126 +7,163 @@ typedef int bool;
 
 typedef int KEY_TYPE;
 
-typedef struct aux { 
-	KEY_TYPE value;
-	struct aux *left, *right;
+typedef struct aux {
+  KEY_TYPE value;
+  struct aux *left, *right;
 } NODE;
 
-NODE* new_tree() {
-	return NULL;
+NODE *new_tree() { return NULL; }
+
+NODE *create_node(KEY_TYPE value) {
+  NODE *new = (NODE *)malloc(sizeof(NODE));
+  new->value = value;
+  new->left = NULL;
+  new->right = NULL;
+  return new;
 }
 
-NODE* create_node(KEY_TYPE value) {
-	NODE* new = (NODE*)malloc(sizeof(NODE));
-	new->value = value;
-	new->left = NULL;
-	new->right = NULL;
-	return new;
+NODE *insert(NODE *root, KEY_TYPE value) {
+  if (root == NULL)
+    return create_node(value);
+  if (value < root->value) {
+    root->left = insert(root->left, value);
+  } else {
+    root->right = insert(root->right, value);
+  }
+  return root;
 }
 
-NODE* insert(NODE* root, KEY_TYPE value) {
-	if (root == NULL) return create_node(value);
-	if (value < root->value) {
-		root->left = insert(root->left, value);
-	} else {
-		root->right = insert(root->right, value);
-	}
-	return root;
+NODE *search(NODE *root, KEY_TYPE value) {
+  if (root == NULL)
+    return NULL;
+  if (root->value == value)
+    return root;
+  if (value < root->value)
+    return search(root->left, value);
+  return search(root->right, value);
 }
 
-NODE* search(NODE* root, KEY_TYPE value) {
-	if (root == NULL) return NULL;
-	if (root->value == value) return root;
-	if (value < root->value) return search(root->left, value);
-	return search(root->right, value);
+int size(NODE *root) {
+  if (root == NULL)
+    return 0;
+  return size(root->left) + 1 + size(root->right);
 }
 
-int size(NODE* root) {
-	if (root == NULL) return 0;
-	return size(root->left) + 1 + size(root->right);
+NODE *search_prev(NODE *root, KEY_TYPE value) {
+  if (root->left->value == value || root->right->value == value)
+    return root;
+  if (value < root->value)
+    return search_prev(root->left, value);
+  return search_prev(root->right, value);
 }
 
-NODE* search_prev(NODE* root, KEY_TYPE value) {
-	if (root->left->value == value || root->right->value == value) return root;
-	if (value < root->value) return search_prev(root->left, value);
-	return search_prev(root->right, value);
+NODE *delete(NODE *root, KEY_TYPE value) {
+  if (root == NULL)
+    return root;
+
+  NODE *node = search(root, value);
+  if (node == NULL)
+    return root;
+
+  NODE *parent = search_prev(root, value);
+  NODE *x, *y;
+
+  if (!node->left || !node->right) {
+    if (!node->left)
+      y = node->right;
+    else
+      y = node->left;
+  } else {
+    x = node;
+    y = node->left;
+    while (y->right) {
+      x = y;
+      y = y->right;
+    }
+    if (x != node) {
+      x->right = y->left;
+      y->left = node->left;
+    }
+    y->right = node->right;
+  }
+  if (!parent) {
+    free(node);
+    return y;
+  }
+  if (value < parent->value)
+    parent->left = y;
+  else
+    parent->right = y;
+  free(node);
+  return root;
 }
 
-NODE* delete(NODE* root, KEY_TYPE value) {
-	if (root == NULL) return false;
+// Alternative implementation
+//  if (root == NULL)
+//    return root;
+//  if (value < root->value)
+//    root->left = delete (root->left, value);
+//  else if (value > root->value)
+//    root->right = delete (root->right, value);
+//  else {
+//    if (root->left == NULL)
+//      return root->right;
+//    if (root->right == NULL)
+//      return root->left;
+//    NODE *min = root->right;
+//    while (min->left != NULL)
+//      min = min->left;
+//    root->value = min->value;
+//    root->right = delete (root->right, min->value);
+//  }
+//  return root;
 
-	NODE* temp_node;
-	NODE* unwanted_node = search(root, value);
-
-	if (value == root->value) {
-		if (root->left == NULL && root->right == NULL) {
-			free(root);
-			return NULL;
-		}
-
-		if (root->right != NULL) root = root->right;
-		else root = root->left;
-		
-		NODE* temp_node = root;
-		while (temp_node->left != NULL) temp_node = temp_node->left;
-		temp_node->left = unwanted_node->left;
-	} else {
-		NODE* prev = search_prev(root, value);
-		
-		int descendents;
-		if (unwanted_node->left == NULL && unwanted_node->right == NULL) {
-			descendents = 0;
-		} else if (unwanted_node->right == NULL && unwanted_node->left != NULL) {
-			descendents = 1;
-		} else if (unwanted_node->left == NULL && unwanted_node->right != NULL) {
-			descendents = 2;
-		} else {
-			descendents = 3;
-		}
-	}
-	free(unwanted_node);
-	return root;
-}
-
-void print(NODE* root) {
-	if (root != NULL) {
-		printf("%d", root->value);
-		printf("(");
-		print(root->left);
-		print(root->right);
-		printf(")");
-	}
+void print(NODE *root) {
+  if (root != NULL) {
+    printf("%d", root->value);
+    printf("(");
+    print(root->left);
+    print(root->right);
+    printf(")");
+  }
 }
 
 int main(void) {
-	NODE* tree = new_tree();
+  NODE *tree = new_tree();
 
-	tree = insert(tree, 15);
-	tree = insert(tree, 20);
-	tree = insert(tree, 23);
-	tree = insert(tree, 19);
-	tree = insert(tree, 8);
-	tree = insert(tree, 3);
-	tree = insert(tree, 12);
+  tree = insert(tree, 15);
+  tree = insert(tree, 20);
+  tree = insert(tree, 23);
+  tree = insert(tree, 19);
+  tree = insert(tree, 8);
+  tree = insert(tree, 3);
+  tree = insert(tree, 12);
 
-	print(tree);
-	printf("\n");
-	
-	bool r;
-	if (search(tree, 23)) r = true;
-	else r = false;
-	printf("Contain 23: %b\n", r);
+  print(tree);
+  printf("\n");
 
-	if (search(tree, 99)) r = true;
-	else r = false;
-	printf("Contain 99: %b\n", r);
-	
-	if (search(tree, 89)) r = true;
-	else r = false;
-	printf("Contain 89: %b\n", r);
+  bool r;
+  if (search(tree, 23))
+    r = true;
+  else
+    r = false;
+  printf("Contain 23: %b\n", r);
 
-	printf("Size: %d\n", size(tree));
+  if (search(tree, 99))
+    r = true;
+  else
+    r = false;
+  printf("Contain 99: %b\n", r);
 
-	tree = delete(tree, 23);
-	print(tree);
-} 
+  if (search(tree, 89))
+    r = true;
+  else
+    r = false;
+  printf("Contain 89: %b\n", r);
+
+  printf("Size: %d\n", size(tree));
+
+  tree = delete (tree, 23);
+  print(tree);
+  printf("\n");
+}
